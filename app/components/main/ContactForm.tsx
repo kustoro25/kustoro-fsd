8d24862a25875709cabb3c97bdb2e8a9c5cb7142
kustoro25 <kustoroterbatas@gmail.com>
2026-02-05 22:30:30 +0700

    update form email

--- app/components/main/ContactForm.tsx
+++ app/components/main/ContactForm.tsx
@@ -315,7 +315,7 @@ export default function ContactForm() {
                   <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                   <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
-                <span>your.email@example.com</span>
+                <span>kustoroterbatas@gmail.com</span>
               </a>
               <span className="text-gray-600 hidden sm:inline">•</span>
               <div className="flex items-center gap-2 text-gray-300">
